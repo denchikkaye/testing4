b1de3f14e9a80c489db05a0365e914ca6e1ab4c9
denchikkaye <121409473+denchikkaye@users.noreply.github.com>
2024-03-26 20:40:52 +0300

    Update Locators.java

--- src/main/java/utils/Locators.java
+++ src/main/java/utils/Locators.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.By;
 public interface Locators {
 
     public interface MainPage {
-        By BUTTON = By.xpath("(//div[contains(text(),'Партнерская программа')])[1]");
+        By BUTTON = By.xpath("(//div[@class='flex items-center justify-center'][contains(text(),'Хостинг')])[1]");
+        By BUTTON2 = By.xpath("(//div[contains(text(),'Партнерская программа')])[1]");
     }
 
     public interface SearchResultPage {
